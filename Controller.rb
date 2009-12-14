a53d7964e3c128d26fc9b1f1eb8675ee455a97d1
James Pozdena <jpoz@jpoz.net>
2009-12-13 16:59:01 -0800

    First commit basic MacRuby app

---
+++ Controller.rb
@@ -0,0 +1,14 @@
+# Controller.rb
+# Almaz
+#
+# Created by James Pozdena on 12/13/09.
+# Copyright 2009 JPoz. All rights reserved.
+
+
+class Controller
+	attr_writer :dataTableView
+	
+	def getData(sender)
+		puts "Getting data"
+	end
+end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Almaz
+//
+//  Created by James Pozdena on 12/13/09.
+//  Copyright JPoz 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
